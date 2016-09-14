aef8b3ff573cef8b990ea7ce896920928c8a4986
MauricioLudwig <mauricio.ludwig@outlook.com>
2016-09-14 14:08:38 +0200

    La till static string name = "player"

--- Hangman 1.0/Program.cs
+++ Hangman 1.0/Program.cs
@@ -16,6 +16,7 @@ namespace Hangman_1._0
         static string story;
         static string guessedWord = string.Empty;
         static string jimmysKalsonger = "Svarta";
+        static string name = "player";
 
         static void Main(string[] args)
         {
